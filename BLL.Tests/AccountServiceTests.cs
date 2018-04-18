aa6157fa8987e6d8528d0f1c97cc8c3bf331c662
kalenikm <kalenikmisha@gmail.com>
2018-04-18 20:30:43 +0300

    Tests with moq were added

--- BLL.Tests/AccountServiceTests.cs
+++ BLL.Tests/AccountServiceTests.cs
@@ -1,4 +1,9 @@
-﻿using BLL.ServiceImplementation;
+﻿using System;
+using System.Collections.Generic;
+using BLL.Interface;
+using BLL.Interface.Interfaces;
+using BLL.ServiceImplementation;
+using DAL.Interface;
 using DAL.Interface.Interfaces;
 using NUnit.Framework;
 using Moq;
@@ -8,10 +13,43 @@ namespace BLL.Tests
     [TestFixture]
     public class AccountServiceTests
     {
-        [Test]
-        public void AccountServiceTest()
+        [TestCase]
+        public void AccountService_StorageSaveTest()
         {
+            var repositoryMock = new Mock<IAccountStorage>(MockBehavior.Strict);
+            repositoryMock.Setup(rep => rep.Load(new Account[0]));
 
+            var accountService = new AccountService(repositoryMock.Object);
+            accountService.Save();
+
+            repositoryMock.Verify(rep => rep.Load(new Account[0]));
+        }
+        [TestCase]
+        public void AccountService_StorageLoadTest()
+        {
+            var repositoryMock = new Mock<IAccountStorage>(MockBehavior.Strict);
+            repositoryMock.Setup(rep => rep.Read()).Returns(() => new Account[0]);
+
+            var accountService = new AccountService(repositoryMock.Object);
+            accountService.Load();
+
+            repositoryMock.Verify(rep => rep.Read());
+        }
+
+        [TestCase("1", "1", AccountType.Base)]
+        [TestCase("2", "2", AccountType.Platinum)]
+        public void AccountNumberCreatorTest(string firstname, string lastname, AccountType type)
+        {
+            var repositoryMock = new Mock<IAccountStorage>();
+
+            var numberCreatorMock = new Mock<IAccountNumberCreator>(MockBehavior.Strict);
+            numberCreatorMock.Setup(service => service.CreateNumber()).Returns(1);
+
+            var accountService = new AccountService(repositoryMock.Object);
+
+            accountService.CreateAccount(firstname, lastname, type, numberCreatorMock.Object);
+
+            numberCreatorMock.Verify(service => service.CreateNumber());
         }
     }
 }
